f082766e516a184d568b6ec0768b225a93a3775f
bulbo-bolson <bolsonbulbo@gmail.com>
2020-04-14 14:42:10 +0200

    git and jenkins integration

--- jenkins-course/jenkins/trigger-job.sh
+++ jenkins-course/jenkins/trigger-job.sh
@@ -1,4 +1,5 @@
 JENKINS_URL="localhost:8080"
-crumb=$(curl -u "jenkins:1234" -s 'http://$JENKINS_URL:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-#curl -u "jenkins:1234" -H "$crumb" -X POST http://$JENKINS_URL:8080/job/ENV/build?delay=0sec
-curl -u "jenkins:1234" -H "$crumb" -X POST  http://$JENKINS_URL:8080/job/backup-db-to-aws/buildWithParameters?DB_HOST=db_host&DB_NAME=testdb&BUCKET_NAME=bulbo-jenkins-udemy
+crumb=$(curl -u "jenkins:1234" -s 'http://'$JENKINS_URL'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+# curl -u "jenkins:1234" -H "$crumb" -X POST http://$JENKINS_URL/job/ENV/build?delay=0sec
+# curl -u "jenkins:1234" -H "$crumb" -X POST  http://$JENKINS_URL/job/db-ansible-users/buildWithParameters?EDAD=22
+curl -u "jenkins:1234" -H "$crumb" -X POST  http://$JENKINS_URL/job/backup-db-to-aws/buildWithParameters?DB_HOST=db_host&DB_NAME=testdb&BUCKET_NAME=bulbo-jenkins-udemy
