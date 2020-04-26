4768150077c5447270c4e893fe47f7edd1b231f1
bulbo-bolson <bolsonbulbo@gmail.com>
2020-04-26 13:59:34 +0200

    modifico volumen options test stage

---
+++ jenkins-course/jenkins/pipeline/README.md
@@ -0,0 +1,1 @@
+app java con su piipeline
--- jenkins-course/jenkins/pipeline/jenkins/test/test.sh
+++ jenkins-course/jenkins/pipeline/jenkins/test/test.sh
@@ -4,5 +4,7 @@ echo "***********************"
 echo "Testing"
 echo "***********************"
 
-docker run --rm -v /root/.m2:/root/.m2 -v "$PWD/simple-java-maven-app:/app" -w /app maven:3-alpine "${@}"
+# docker run --rm -v /root/.m2:/root/.m2 -v "$PWD/simple-java-maven-app:/app" -w /app maven:3-alpine "${@}"
+# al usar el socket de la maquina remota, docker se confunde con la ruta y PWD no corresponde a lo que deberia, le ponemos la ruta completa de la maquina local
+docker run --rm -v /root/.m2:/root/.m2 -v "/home/curro/gdrive/src/fun/docker-projects/jenkins-course/jenkins/pipeline/simple-java-maven-app:/app" -w /app maven:3-alpine "${@}"
 
